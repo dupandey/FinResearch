95908dc5f74005cd1e2b9e7ddf858e5be1ab0f82
dupandey <qsangam@hotmail.com>
2020-03-22 07:17:40 +0530

    Added the database project

---
+++ Database/ISs.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[ISs](
+	[ISId] [bigint] IDENTITY(1,1) NOT NULL,
+	[CompanyId] [int] NOT NULL,
+	[CategoryId] [int] NOT NULL,
+	[Payload] [nvarchar](max) NULL,
+	[IsActive] [bit] NOT NULL,
+	[CreatedDate] [datetime] NULL,
+	[ModifiedDate] [datetime] NULL,
+ CONSTRAINT [PK_ISs] PRIMARY KEY CLUSTERED 
+(
+	[ISId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
---
+++ Database/ThemeMasterIss.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE [dbo].[ThemeMasterIss](
+	[Id] [bigint] IDENTITY(1,1) NOT NULL,
+	[CompanyId] [int] NOT NULL,
+	[CategoryId] [int] NOT NULL,
+	[Payload] [nvarchar](max) NULL,
+	[IsActive] [bit] NOT NULL,
+	[CreatedDate] [datetime] NULL,
+	[ModifiedDate] [datetime] NULL,
+ CONSTRAINT [PK_ThemeMasterIss] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+
+
